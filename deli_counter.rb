74e038368f5634ab70a881edc883f25aa724c154
Kyle Graden <kylegraden3@gmail.com>
2019-03-26 21:03:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,11 +16,11 @@ def line(line)
   if line.length == 0;
     puts "The line is currently empty.";
   else;
-    var = "The line is currently: "
+    var = "The line is currently:"
     var2 = ""
     line.each_with_index do |name, index|;
       index_plus_one = index + 1;
-    var2 = var.concat("#{index_plus_one}. #{name} ");
+    var2 = var.concat(" #{index_plus_one}. #{name}");
     end;
     puts var2
     # binding.pry
